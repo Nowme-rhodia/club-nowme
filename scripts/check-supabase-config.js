627c3243a66b863eb5e5cb94847f8a975bcca91e
Nowme-rhodia <nowme.club@gmail.com>
2025-07-23 17:48:49 +0200

    Updated check-supabase-config.js

--- scripts/check-supabase-config.js
+++ scripts/check-supabase-config.js
@@ -3,8 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 // Charger le fichier .env
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
