1e75acf13ae2c8bb601d25b8968c67c7a5fd5896
Adminrhodia <nowme.club@gmail.com>
2026-01-26 12:06:54 +0100

    erreur create offer

--- src/pages/partner/CreateOffer.tsx
+++ src/pages/partner/CreateOffer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { supabase } from '../../lib/supabase';
 import { useAuth } from '../../lib/auth';
